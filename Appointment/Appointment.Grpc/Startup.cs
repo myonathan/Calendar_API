24dbf35fbeaa3f0349a68647aafc7e9b8b8c14cb
yoyo <yoyo.mitrais@gmail.com>
2022-03-19 09:20:55 +0700

    Enabled list binder

--- Appointment/Appointment.Grpc/Startup.cs
+++ Appointment/Appointment.Grpc/Startup.cs
@@ -83,8 +83,8 @@ namespace Appointment.Grpc
             TypeDescriptor.AddAttributes(typeof(AppointmentModel), new TypeConverterAttribute(typeof(ToAppoinmentResponse)));
             TinyMapper.Bind<AppointmentModel, AppointmentResponse>();
 
-            //TypeDescriptor.AddAttributes(typeof(List<AppointmentModel>), new TypeConverterAttribute(typeof(ToAppointmentListResponse)));
-            //TinyMapper.Bind<List<AppointmentModel>, List<AppointmentResponse>>();
+            TypeDescriptor.AddAttributes(typeof(List<AppointmentModel>), new TypeConverterAttribute(typeof(ToAppointmentListResponse)));
+            TinyMapper.Bind<List<AppointmentModel>, List<AppointmentResponse>>();
 
             TypeDescriptor.AddAttributes(typeof(AppointmentRequest), new TypeConverterAttribute(typeof(ToAppointmentModel)));
             TinyMapper.Bind<AppointmentRequest, AppointmentModel>();
